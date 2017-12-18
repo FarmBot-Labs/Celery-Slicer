8acc67ed6f2cf860e7a1339cd7264ade25e4a9c6
Rick Carlino <rick.carlino@gmail.com>
2017-12-18 10:48:05 -0600

    Convert jsCase to elixir_case

--- 1_elixir_version/lib/slicer.ex
+++ 1_elixir_version/lib/slicer.ex
@@ -12,43 +12,43 @@ defmodule Slicer do
     CSHeap.dump(heap)
   end
 
-  def allocate(heap, sequence, parentAddr) do
+  def allocate(heap, sequence, parnt_addr) do
     {:ok, addr} = CSHeap.allot(heap, sequence["kind"])
-    CSHeap.put(heap, addr, "🔗parent", parentAddr)
-    iterateOverBody(heap, sequence, addr) # *ALWAYS* before iterateOverArgs
-    iterateOverArgs(heap, sequence, addr)
+    CSHeap.put(heap, addr, "🔗parent", parnt_addr)
+    iterateOverBody(heap, sequence, addr) # *ALWAYS* before iterate_over_args
+    iterate_over_args(heap, sequence, addr)
     addr
   end
 
-  def iterateOverArgs(heap, node, parentAddr) do
+  def iterate_over_args(heap, node, parnt_addr) do
     args = node["args"]
     keys = Map.keys(args)
     mapper = fn key ->
         v = args[key]
-        if isCeleryScript(v) do
-          CSHeap.put(heap, parentAddr, "🔗" <> key, allocate(heap, v, parentAddr))
+        if is_celery(v) do
+          CSHeap.put(heap, parnt_addr, "🔗" <> key, allocate(heap, v, parnt_addr))
         else
-          CSHeap.put(heap, parentAddr, key, v)
+          CSHeap.put(heap, parnt_addr, key, v)
         end
       end
     Enum.map(keys, mapper)
   end
 
-  def iterateOverBody(heap, s, parentAddr) do
+  def iterateOverBody(heap, s, parnt_addr) do
     body = s["body"] || []
-    (length(body) > 0) && CSHeap.put(heap, parentAddr, "🔗body", parentAddr + 1)
-    recurseIntoBody(heap, 0, parentAddr, body)
+    (length(body) > 0) && CSHeap.put(heap, parnt_addr, "🔗body", parnt_addr + 1)
+    recurse_into_body(heap, 0, parnt_addr, body)
   end
 
-  def recurseIntoBody(heap, index, parent, list) do
+  def recurse_into_body(heap, index, parent, list) do
     item = Enum.at(list, index)
     if(item) do
       me = allocate(heap, item, parent)
-      recurseIntoBody(heap, index + 1, me, list)
+      recurse_into_body(heap, index + 1, me, list)
     end
   end
 
-  @compile {:inline, isCeleryScript: 1}
-  def isCeleryScript(%{"kind" => _kind, "args" => _args}), do: true
-  def isCeleryScript(_), do: false
+  @compile {:inline, is_celery: 1}
+  def is_celery(%{"kind" => _kind, "args" => _args}), do: true
+  def is_celery(_), do: false
 end
